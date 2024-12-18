1063dea191665eab39a2b2fdd42d18612ccbd149
Jurgen Werkmeister <jwerkmei@gmail.com>
2024-12-18 10:08:10 -0300

    Cambios clase 4

--- app.py
+++ app.py
@@ -2,52 +2,69 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 from openai import OpenAI
 from dotenv import load_dotenv
+from db import db, db_config
+from models import User, Message
 
 load_dotenv()
 
 client = OpenAI()
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
+db_config(app)
+
 
 @app.route('/')
 def index():
     return render_template('landing.html')
 
 
+
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
+    user = db.session.query(User).first()
+
     if request.method == 'GET':
-        return render_template('chat.html')
+        return render_template('chat.html', messages=user.messages)
 
-    intent1 = request.form.get('intent1')
-    intent2 = request.form.get('intent2')
-    intent3 = request.form.get('intent3')
+    intent = request.form.get('intent')
 
-    # Verificar cuál no es None
-    if intent1 is not None:
-        intent=intent1
-    elif intent2 is not None:
-        intent=intent2
-    elif intent3 is not None:
-        intent=intent3
+    intents = {
+        'Quiero tener suerte': 'Recomiéndame una película',
+        'Terror': 'Recomiéndame una película de terror',
+        'Acción': 'Recomiéndame una película de acción',
+        'Comedia': 'Recomiéndame una película de comedia',
+        'Enviar': request.form.get('message')
+    }
 
-    chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "Eres un chatbot que recomienda películas, te llamas 'Next Moby'. Tu rol es responder recomendaciones de manera breve y concisa.",
-                },
-                {
-                    "role": "user",
-                    "content": "Recomiéndame una película de "+ intent,
-                }
-            ],
+    if intent in intents:
+        user_message = intents[intent]
+
+        # Guardar nuevo mensaje en la BD
+        db.session.add(Message(content=user_message, author="user", user=user))
+        db.session.commit()
+
+        messages_for_llm = [{
+            "role": "system",
+            "content": "Eres un chatbot que recomienda películas, te llamas 'Next Moby'. Tu rol es responder recomendaciones de manera breve y concisa. No repitas recomendaciones.",
+        }]
+
+        for message in user.messages:
+            messages_for_llm.append({
+                "role": message.author,
+                "content": message.content,
+            })
+
+        chat_completion = client.chat.completions.create(
+            messages=messages_for_llm,
             model="gpt-4o",
             temperature=1
         )
 
-    model_recommendation = chat_completion.choices[0].message.content
-    return render_template('chat.html', recommendation=model_recommendation)
+        model_recommendation = chat_completion.choices[0].message.content
+        db.session.add(Message(content=model_recommendation, author="assistant", user=user))
+        db.session.commit()
+
+        return render_template('chat.html', messages=user.messages)
 
 
 @app.route('/user/<username>')
@@ -62,20 +79,26 @@ def user(username):
 
 @app.post('/recommend')
 def recommend():
+    user = db.session.query(User).first()
     data = request.get_json()
     user_message = data['message']
+    new_message = Message(content=user_message, author="user", user=user)
+    db.session.add(new_message)
+    db.session.commit()
+
+    messages_for_llm = [{
+        "role": "system",
+        "content": "Eres un chatbot que recomienda películas, te llamas 'Next Moby'. Tu rol es responder recomendaciones de manera breve y concisa. No repitas recomendaciones.",
+    }]
+
+    for message in user.messages:
+        messages_for_llm.append({
+            "role": message.author,
+            "content": message.content,
+        })
 
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "Eres un chatbot que recomienda películas, te llamas 'Next Moby'. Tu rol es responder recomendaciones de manera breve y concisa.",
-            },
-            {
-                "role": "user",
-                "content": user_message,
-            }
-        ],
+        messages=messages_for_llm,
         model="gpt-4o",
     )
 
---
+++ models.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import relationship
+from db import db
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Text,
+    DateTime,
+    ForeignKey,
+)
+from datetime import datetime
+
+
+class User(db.Model):
+   __tablename__ = "users"
+
+   id = Column(Integer, primary_key=True, autoincrement=True)
+   created_at = Column(DateTime, default=datetime.utcnow)
+   email = Column(String, nullable=False, unique=True)
+   nombre = Column(String, nullable=False, unique=False)
+   generos_favoritos = Column(String, nullable=False, unique=False)
+
+   messages = relationship("Message", back_populates="user")
+
+
+class Message(db.Model):
+   __tablename__ = "messages"
+   id = Column(Integer, primary_key=True, autoincrement=True)
+   created_at = Column(DateTime, default=datetime.utcnow)
+   content = Column(Text, nullable=False)
+   author = Column(String, nullable=False)  # 'user' or 'assistant'
+   user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+   user = relationship("User", back_populates="messages")
